7148ebd05fde04916b9012228f0c18a22ae71d71
Waraaaa <130883334+Waraaaa@users.noreply.github.com>
2025-04-11 13:38:41 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class _1oiiaio {
+
+public static void huh(int[] noodleSoup){
+
+for(int among=0;among<noodleSoup.length-1;among++){
+for(int sus=0;sus<=noodleSoup.length;sus++){ 
+if(noodleSoup[among]<noodleSoup[sus]){ 
+int yeet = noodleSoup[sus]; 
+noodleSoup[sus] = noodleSoup[among];
+noodleSoup[among] = yeet;
+}}}}
+
+public static void main(String[] wow){
+int[] eww = {42, 1337, 7, 0, -1};
+huh(eww);
+for(int iAmInevitable = 0;iAmInevitable<eww.length;iAmInevitable++)
+System.out.print(eww[iAmInevitable]+" ");
+}}
