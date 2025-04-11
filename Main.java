e7d0fefb7eb1ef5aa7d2684e23e74468335c9253
Waraaaa <130883334+Waraaaa@users.noreply.github.com>
2025-04-11 13:57:03 +0700

    Update Main.java
    is this oki paisen

--- Main.java
+++ Main.java
@@ -1,18 +1,28 @@
-public class _1oiiaio {
+public class SortDemo {
 
-public static void huh(int[] noodleSoup){
+    // Method to sort an array in ascending order
+    public static void sortArray(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            // Start from i + 1 to avoid comparing the same pair twice
+            for (int j = i + 1; j < array.length; j++) {
+                // Use > to sort in ascending order
+                if (array[i] > array[j]) {
+                    // Swap elements
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
 
-for(int among=0;among<noodleSoup.length-1;among++){
-for(int sus=0;sus<=noodleSoup.length;sus++){ 
-if(noodleSoup[among]<noodleSoup[sus]){ 
-int yeet = noodleSoup[sus]; 
-noodleSoup[sus] = noodleSoup[among];
-noodleSoup[among] = yeet;
-}}}}
-
-public static void main(String[] wow){
-int[] eww = {42, 1337, 7, 0, -1};
-huh(eww);
-for(int iAmInevitable = 0;iAmInevitable<eww.length;iAmInevitable++)
-System.out.print(eww[iAmInevitable]+" ");
-}}
+    public static void main(String[] args) {
+        int[] numbers = {42, 1337, 7, 0, -1};
+        sortArray(numbers);
+        
+        // Print the sorted array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
